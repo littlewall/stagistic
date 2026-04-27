371a761945d66808f18936deb495dc268accb348
Milan Zítka <milan@digitalvision.cz>
2026-04-27 14:52:03 +0200

    refactor(shared): move nodeId into ids/ subdirectory

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,8 +2,8 @@
  * Shared generic utilities.
  */
 
+export * from './ids/nodeId';
 export * from './ids/uuidv7';
-export * from './nodeId';
 export * from './utils/number';
 export * from './utils/object';
 export * from './utils/string';
