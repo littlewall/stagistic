ef463a672c1138aedbc74d736c179c93b4dca503
Milan Zítka <milan@digitalvision.cz>
2026-05-15 16:52:23 +0200

    refactor: export new handler group types from settings index

--- packages/app-routes/src/routes/script/editor/settings/index.ts
+++ packages/app-routes/src/routes/script/editor/settings/index.ts
@@ -2,6 +2,11 @@ export {normalizeSettingsOverride} from './normalizeSettingsOverride';
 export {ScriptEditorSettingsPanel} from './ScriptEditorSettingsPanel';
 export type {
     BlockSettingsPatch,
+    ElementsHandlers,
+    PageLayoutHandlers,
+    PageSettingsPatch,
     ScriptEditorSettingsPanelProps,
+    StructureHandlers,
     StructureSettingsPatch,
+    VisualPreferencesHandlers,
 } from './types';
