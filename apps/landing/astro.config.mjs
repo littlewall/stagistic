348367c84405ef6cfeb3b3a226de971c359f50b5
Milan Zítka <milan@digitalvision.cz>
2026-08-17 13:40:17 +0200

    fix: ensure trailing slash configuration is set to 'never' in Astro config

--- apps/landing/astro.config.mjs
+++ apps/landing/astro.config.mjs
@@ -4,6 +4,7 @@ import {defineConfig} from 'astro/config';
 export default defineConfig({
     site: 'https://stagistic.com',
     integrations: [react()],
+    trailingSlash: 'never',
     vite: {
         css: {
             modules: {
